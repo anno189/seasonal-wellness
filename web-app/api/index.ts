deb3221c454afb32f72329fb18dd9ac11fd878c8
FartherQuant <fartherquant@gmail.com>
2026-07-24 09:00:27 +0800

    update vercel
    update vercel

--- web-app/api/index.ts
+++ web-app/api/index.ts
@@ -1,9 +1,9 @@
 /**
  * Vercel deploy entry handler, for serverless deployment, please don't modify this file
  */
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { IncomingMessage, ServerResponse } from 'http';
 import app from '../src/server/app.js';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: IncomingMessage, res: ServerResponse) {
   return app(req, res);
 }
